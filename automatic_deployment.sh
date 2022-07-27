ebc097f4fa2b6b7b37a93cedc43873513b51b5c2
Your Name <testse@te.ca>
2022-07-27 14:50:35 +0200

    Add CD

--- automatic_deployment.sh
+++ automatic_deployment.sh
@@ -7,18 +7,13 @@ mkdir -p $DEPLOY_PATH
 
 cd $DEPLOY_PATH
 git clone https://github.com/NolanBoukachab/time_tracker tt
-echo "1"
 cd tt
-echo "11"
-heroku git:remote -a $1
-echo "2"
+heroku git:remote -a timetrackin
 git add -A .
-echo "3"
 git commit -m "deploy"
-echo "4"
 git push heroku main
 
 cd -
 rm -rf $DEPLOY_PATH
 
-echo "🚀  https://$1.herokuapp.com"
+echo "🚀  https://timetrackin.herokuapp.com"
