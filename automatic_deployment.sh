25a425d3c7226199ba830ee8bfdcb5b403b8e995
Your Name <testse@te.ca>
2022-07-28 15:39:15 +0200

    Update deployment script

--- automatic_deployment.sh
+++ automatic_deployment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 git remote add heroku "https://${HEROKU_USERNAME}:${HEROKU_TOKEN}@git.heroku.com/timetrackin.git"
-git push heroku HEAD:main
+git push heroku HEAD:refs/heads/main
