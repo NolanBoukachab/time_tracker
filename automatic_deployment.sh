fa58b86da88428fb3e2bc4bedb101e6af789bd9e
Your Name <testse@te.ca>
2022-07-28 09:40:46 +0200

    Fix the deployement

--- automatic_deployment.sh
+++ automatic_deployment.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-DEPLOY_PATH=/tmp/heroku-deploy-`date +%s`/
-git config --global user.email "ci.cd@deploy.com"
-git config --global user.name "ci.cd"
-
-rm -rf $DEPLOY_PATH
-mkdir -p $DEPLOY_PATH
-
-cd $DEPLOY_PATH
-git clone https://github.com/NolanBoukachab/time_tracker tt
-cd tt
-heroku git:remote -a timetrackin
-git add -A .
-git commit -m "deploy"
+git remote add heroku https://${HEROKU_USERNAME}:${HEROKU_TOKEN}@git.herokuapp.com/timetrackin
 git push heroku main
-
-cd -
-rm -rf $DEPLOY_PATH
-
-echo "🚀  https://timetrackin.herokuapp.com"
