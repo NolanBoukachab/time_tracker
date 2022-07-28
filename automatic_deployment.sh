6b2fa22ef1101e488f09c02491aa868d113c8c7c
Your Name <testse@te.ca>
2022-07-28 10:16:13 +0200

    Fix the CD

--- automatic_deployment.sh
+++ automatic_deployment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo ${HEROKU_USERNAME}
 echo ${HEROKU_TOKEN}
+echo ${"Test"}
 git remote add heroku https://${HEROKU_USERNAME}:${HEROKU_TOKEN}@git.herokuapp.com/timetrackin
 git push heroku main
